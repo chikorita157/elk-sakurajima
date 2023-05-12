362f63ac9f8fc24cdd58f5bb10d175d375a4179b
James Moy <chikorita157@chikorita157.com>
2023-05-12 08:20:38 -0400

    Update app.vue
    update utl

--- app.vue
+++ app.vue
@@ -7,7 +7,7 @@ const route = useRoute()
 if (process.server && !route.path.startsWith('/settings')) {
   useHead({
     meta: [
-      { property: 'og:url', content: `https://elk.sakurajima.moe${route.path}` },
+      { property: 'og:url', content: `https://elk.sakurajima.social${route.path}` },
     ],
   })
 }
